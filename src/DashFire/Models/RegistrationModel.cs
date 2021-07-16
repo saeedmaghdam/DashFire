c3f0c9279704d42a9c4ccc843af476dc01d3ca23
saeedmaghdam <me.register@outlook.com>
2021-07-16 21:57:24 +0430

    #18 Refactored, changed rabbit models to internal.

--- src/DashFire/Models/RegistrationModel.cs
+++ src/DashFire/Models/RegistrationModel.cs
@@ -2,32 +2,20 @@
 
 namespace DashFire.Models
 {
-    /// <summary>
-    /// Job's registration model
-    /// </summary>
-    public class RegistrationModel
+    internal class RegistrationModel
     {
-        /// <summary>
-        /// Full name of the job.
-        /// </summary>
         public string Key
         {
             get;
             set;
         }
 
-        /// <summary>
-        /// Job's instance id.
-        /// </summary>
         public string InstanceId
         {
             get;
             set;
         }
-
-        /// <summary>
-        /// Parameters list including parameter name, display name, description and parameter type.
-        /// </summary>
+        
         public List<JobParameterModel> Parameters
         {
             get;
